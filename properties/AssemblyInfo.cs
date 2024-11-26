8ff46a7ada4c7f6b7a13f512dd96de4cf58f75c4
NepuShiro <nepushiro@nepunep.xyz>
2024-11-26 07:04:51 -0600

    Completely reorder Repo

---
+++ properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("ResoniteSlotInspectorCounter")]
+[assembly: AssemblyProduct("ResoniteSlotInspectorCounter")]
+[assembly: AssemblyDescription("Description of your mod")]
+[assembly: AssemblyCopyright("Copyright © 2024 ExampleAuthor")]
+[assembly: AssemblyVersion("1.3.0")]
+[assembly: AssemblyFileVersion("1.3.0")]
