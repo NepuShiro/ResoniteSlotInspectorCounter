2ebdc5eb63bd1b3c8ec40c38d92d20af249cc075
NepuShiro <nepushiro@nepunep.xyz>
2024-11-26 20:38:55 -0600

    Update AssemblyInfo

--- properties/AssemblyInfo.cs
+++ properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 
 [assembly: AssemblyTitle("ResoniteSlotInspectorCounter")]
 [assembly: AssemblyProduct("ResoniteSlotInspectorCounter")]
-[assembly: AssemblyDescription("Description of your mod")]
-[assembly: AssemblyCopyright("Copyright © 2024 ExampleAuthor")]
+[assembly: AssemblyDescription("Add a SlotCount to the Inspectors Expand/Collapse buttons")]
+[assembly: AssemblyCopyright("Copyright © 2024 NepuShiro")]
 [assembly: AssemblyVersion("1.3.0")]
 [assembly: AssemblyFileVersion("1.3.0")]
